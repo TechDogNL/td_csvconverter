3deae43bd67db3f14973179301f9e7eeb05c7694
Zippy6872 <159549946+Zippy6872@users.noreply.github.com>
2024-03-06 08:25:46 +0100

    opnieuw beginnen

--- csv-naar-database/resources/views/import.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>Import File</h1>
-<form action="export" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="file" name="file" id="uploadFile"  multiple accept=".csv"> <br> <br>
-    <button type="submit" onclick="checkFile()">Import CSV</button>
-</form>
