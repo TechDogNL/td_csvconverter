ceb854d2b16d7a9fa44bff07351a44182d464ed0
Zippy6872 <159549946+Zippy6872@users.noreply.github.com>
2024-05-06 14:51:56 +0200

    return logs werkt, dit is voor de incident

--- CsvConverter/Csv/src/Components/csvFiles/Importcsv.jsx
+++ CsvConverter/Csv/src/Components/csvFiles/Importcsv.jsx
@@ -1,45 +1,53 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, {useEffect, useMemo, useState, } from "react";
+import Papa from "papaparse";
 import { useDropzone } from 'react-dropzone';
-import Papa from 'papaparse';
-import axios from 'axios';
+import _, { compact, first, isEmpty } from 'lodash';
+
+function Importcsv(){
+  const baseStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px',
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: '#eeeeee',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    color: '#bdbdbd',
+    outline: 'none',
+    transition: 'border .24s ease-in-out',
+    cursor: 'pointer', 
+  };
+  
+  const focusedStyle = {
+    borderColor: '#2196f3'
+  };
+  
+  const acceptStyle = {
+    borderColor: '#00e676'
+  };
+  
+  const rejectStyle = {
+    borderColor: '#ff1744'
+  };
+
+  const boxClickStyle = {
+    position: 'absolute',
+    bottom: '10px',
+    left: '10px',
+    right: '10px',
+    borderTop: '1px solid #ccc',
+    paddingTop: '10px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'right',
+  };
+ 
+  const [uploadedFiles, setUploadedFiles] = useState([]);
 
 
-const baseStyle = {
-  flex: 1,
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  padding: '20px',
-  borderWidth: 2,
-  borderRadius: 2,
-  borderColor: '#eeeeee',
-  borderStyle: 'dashed',
-  backgroundColor: '#fafafa',
-  color: '#bdbdbd',
-  outline: 'none',
-  transition: 'border .24s ease-in-out',
-  cursor: 'pointer', 
-};
-
-const focusedStyle = {
-  borderColor: '#2196f3'
-};
-
-const acceptStyle = {
-  borderColor: '#00e676'
-};
-
-const rejectStyle = {
-  borderColor: '#ff1744'
-};
-
-function Importcsv() {
-    const [uploadedFiles, setUploadedFiles] = useState([]);
-    const [csvData, setcsvData] = useState([]);
-    const [submitting, setSubmitting] = useState(false);
-    const [showExport, setShowExport] = useState(false);
-    const [showImport, setShowImport] = useState(false);
-    
   const {
     acceptedFiles,
     fileRejections,
@@ -48,117 +56,101 @@ function Importcsv() {
     isFocused,
     isDragAccept,
     isDragReject,
-  } = useDropzone({
+} = useDropzone({
     maxFiles: 6, 
     accept: {'text/csv': []}  
-  });
+});
 
-useEffect(() => {
-  console.log("send to export data",csvData);
-  console.log("send to export files",acceptedFiles)
-  if(csvData.length > 0)
-  {
-    setShowExport(true);
-    setShowImport(false);
-  }
-  else{
-    setShowExport(false);
-    setShowImport(true);
-  }
-}, [csvData])
+const style = useMemo(() => ({
+  ...baseStyle,
+  ...(isFocused ? focusedStyle : {}),
+  ...(isDragAccept ? acceptStyle : {}),
+  ...(isDragReject ? rejectStyle : {}),
+}), [
+  isFocused,
+  isDragAccept,
+  isDragReject
+]);
+
+useMemo(() =>{
+  setUploadedFiles(acceptedFiles);
+},[acceptedFiles]);
 
 
-  useMemo(() =>{
-    setUploadedFiles(acceptedFiles);
-  },[acceptedFiles]);
-  
-  const style = useMemo(() => ({
-    ...baseStyle,
-    ...(isFocused ? focusedStyle : {}),
-    ...(isDragAccept ? acceptStyle : {}),
-    ...(isDragReject ? rejectStyle : {}),
-  }), [
-    isFocused,
-    isDragAccept,
-    isDragReject
-  ]);
+  const acceptedFileItems = uploadedFiles.length > 0 ?(
+    acceptedFiles.map(file => (
+       <li key={file.path}>
+           {file.path} - {file.size} bytes
+       </li>
+    ))
+   ) : (
+       <li>No files uploaded</li>
+   )
+   
+   const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+       <li key={file.path}>
+           {file.path} - {file.size} bytes
+           <ul>
+               {errors.map(e => <li key={e.code}>{e.message}</li>)}
+           </ul>
+       </li>
+   ));
 
-  const acceptedFileItems = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
+   {/* handling converting csv to an multidimensional array */}
+function handleCSV () 
+{
+    uploadedFiles.forEach(file => { 
+        Papa.parse(file, {
+            delimiter:";",
+            skipEmptyLines: true,
+            dynamicTyping:true,
+            encoding: "UTF-8",
+            complete: function(results) {
+                console.log('Parsed CSV data:', results.data);    
+            
+                const convertedData = results.data.map(row =>
+                   compact(row).map(cell => {
+                        if (typeof cell === 'string' && !isNaN(parseFloat(cell))) {
+                            return parseFloat(cell.replace(',', '.'));
+                        } else {
+                            return cell;
+                        }
+                    })
+                );
 
-  const fileRejectionItems = fileRejections.map(({ file, errors }) => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-      <ul>
-        {errors.map(e => <li key={e.code}>{e.message}</li>)}
-      </ul>
-    </li>
-  ));
-
-  const resetAll = () => {
-    setUploadedFiles([]);
-    setcsvData([]);
-  }
-
-   const handleParseCSV = () => {
-    setSubmitting(true);
-     uploadedFiles.forEach(file => { 
-      Papa.parse(file, {
-        delimiter:";",
-        skipEmptyLines,
-        header,
-        complete: function(results) {         
-          console.log('Parsed CSV data:', results.data);
-          setcsvData(prevResult => [...prevResult, results.data]);    
-        } 
-      });
+                const convertedToString = convertedData.map(row =>
+                    row.map(cell => String(cell))
+                );
+                const validRows = convertedToString.filter(row => Array.isArray(row) && row.length > 0);
+                
+                setMainArray(prevArray => {
+                    const newArray = [...prevArray, validRows];
+                    setCurrentArray(newArray[index]);
+                    return newArray;
+                })
+            }
+        });
     });
-  };
+};
 
-   //   navigate('export'); //
-    //de waardes naar een controler sturen en daar weer uit halen om een table te gaan maken
-        //nog checken dat het een header heeft
-        //const merged =  merge(results); //dit nog fixen ik moet 2 values meegeven
-        // console.log('merged data',merged);
-  
-  return (
+  return(
     <div>
-  {showImport && ( 
     <section className="container">
-      <h1>Upload je CSV bestand</h1>
-      <div {...getRootProps({style})}>  { /* hier zijn de dropzones handles */ }
-        <input {...getInputProps()} />
-        <p>Klik hier om 1 of meer bestanden te selecteren</p>
-        <em>(Je kunt maximaal 6 bestanden hier neerzetten)</em>
-      </div>
-      <aside>
-        <h4>Geaccepteerde bestanden</h4>
-        <ul>{acceptedFileItems}</ul> 
-        <h4>Afgewezen bestanden</h4>
-        <ul>{fileRejectionItems}</ul>
-      </aside>
-    </section>
-  )}
-     {showImport && (
-    <button onClick={handleParseCSV} >Klik hier om te uploaden</button>
-     )}
-        {/* {showExport && <Exportcsv test={csvData} />} */}
-        {showExport &&(
-          <div>
-            test
-          <button onClick={resetAll}>click</button>
-          </div>
-          
-        )}
-      
+                <h1>Test</h1>
+                <div {...getRootProps({style})}>
+                    <input {...getInputProps()} />
+                    <p>Klik hier om 1 of meer bestanden te selecteren</p>
+                    <em>(Je kunt maximaal 6 bestanden hier neerzetten)</em>
+                </div>
+                <aside>
+                    <h4>Geaccepteerde bestanden</h4>
+                    <ul>{acceptedFileItems}</ul> 
+                    <h4>Afgewezen bestanden</h4>
+                    <ul>{fileRejectionItems}</ul>
+                </aside>
+            </section>
+            <button onClick={handleCSV}>verder</button>
     </div>
-  );
+  )
 }
-{{ /* button onclick()
-const [uploadedFiles, setUploadedFiles] = useState([]);
-const [csvData, setcsvData] = useState([]);
-leeg maken */ }}
-export default Importcsv
+export default Importcsv;
--- CsvConverter/Csv/src/Components/csvFiles/Tabel.jsx
+++ CsvConverter/Csv/src/Components/csvFiles/Tabel.jsx
@@ -1,46 +1,193 @@
-import React, { useEffect, useState } from 'react';
-import { Autocomplete, TextField } from '@mui/material';
-
+import React, {useEffect, useMemo, useState, } from "react";
+import { Table, TableHead, TableBody, TableRow, TableCell, Paper, TableContainer, colors, Autocomplete,Button,Switch,FormControlLabel,Modal,Box,Dialog,DialogTitle,LinearProgress,Typography } from "@mui/material";
+import SettingsIcon from '@mui/icons-material/Settings';
+import TextField from '@mui/material/TextField';
+import {Accordion,AccordionDetails,AccordionSummary,Divider } from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 function Tabel() {
-  const options = ["Option 1", "Option 2", "Option 3"];
-  const [localDisabledOptions, setLocalDisabledOptions] = useState([]);
-  const [globalDisabledOptions, setGlobalDisabledOptions] = useState([]);
-
-
   
-  const handleChange = (event, newValue) => {
-    if (newValue) {
-      // Enable the selected option in the current textfield
-      setLocalDisabledOptions((prevOptions) =>
-        prevOptions.filter((option) => option !== newValue)
-      );
-    }
+  const boxClickStyle = {
+    position: 'absolute',
+    bottom: '10px',
+    left: '10px',
+    right: '10px',
+    borderTop: '1px solid #ccc',
+    paddingTop: '10px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'right',
   };
+ 
 
-  const handleBlur = (event, newValue) => {
-    if (newValue) {
-      // Disable the selected option globally
-      setGlobalDisabledOptions((prevOptions) => [...prevOptions, newValue]);
-    }
-  };
+const [rows,setRows] = useState([]);
+const [mainArray,setMainArray] = useState([]);
+const [index, setIndex] = useState(0)
+const [currentArray,setCurrentArray] = useState([]);
 
+const [disableTable,setDisableTable] = useState(false);
+const [disabledRows,setDisabledRows] = useState([]);
+const [enabledRows,setEnabledRows] = useState([]);
+const [selectedRow, setSelectedRow] = useState(null);
+
+
+const [options,setOptions ] = useState(["productnaam","productnummer", "order1", "order2", "order3","barcode","kleur"]);
+const [disabledOptions,setDisabledOptions] = useState([]);
+const [currentOptions,setCurrentOptions] = useState([])
+const [inputValues, setInputValues] = useState(options.map(() => ''));
+
+const [currentEnabledColumns,setCurrentEnabledColumns] = useState([]); //dit doen voor de tableconfigs zodat het de current als een static heeft
+const [enabledColumns,setEnabledColumns] =  useState([]);
+const [disabledColumns,setDisabledColumns] = useState([]);
+
+const [enabledSwitch,setEnabledSwitch] = useState(false)
+
+const [openDialog,setOpenDialog] = useState(false);
+const [colorArray,setColorArray] = useState([]);
+const [atributes,setAtributes] = useState(["appel","peer","banaan"]);
+
+const count = 0;
+function advancedOptions (){
+  setOpenDialog(true);
+  const colorColumnIndex = currentOptions.indexOf('kleur');
+  if(colorColumnIndex !== -1) {
+      const colorSet = new Set();
+      enabledRows.map((rowIndex) =>{
+      const color = currentArray[rowIndex][colorColumnIndex];
+      colorSet.add(color);
+      })        
+      const uniqueColors = Array.from(colorSet)
+      setColorArray(uniqueColors)
+  } else{
+      //selecteer een kolom met kleur
+      setColorArray([])
+  }
+
+
+}
+const closeDialog = () =>{
+  setOpenDialog(false);
+}
+function reset(){
+
+}
+function toDatabase () {
+
+}
+function toResult () {
+  
+}
 
   return (
     <div>
-        <Autocomplete
-      options={options}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      getOptionDisabled={(option) =>
-        globalDisabledOptions.includes(option) ||
-        localDisabledOptions.includes(option)
-      }
-      renderInput={(params) => (
-        <TextField {...params} label="Select an option" variant="outlined" />
-      )}
-    />
+      <div>
+          <div>
+                <div>
+                    <FormControlLabel control={<Switch checked={enabledSwitch} onChange={()=> setEnabledSwitch(!enabledSwitch)} />} label="Bestaande producten updaten" />
+                    <Button variant="contained" size="small" startIcon={<SettingsIcon/>} onClick={advancedOptions}>Geavanceerde Opties </Button>
+                       <Dialog onClose={closeDialog} open={openDialog} >
+                            <Box sx={{ width: '600px',height: '600px' }}>
+                                <DialogTitle sx={{ borderBottom: '1px solid #ccc',paddingBottom: '10px' }}>Geavanceerde Opties</DialogTitle>
+                                <Autocomplete
+                                // value={}
+                                options={atributes}
+                                renderInput={(params) => (
+                                    <TextField {...params} label="Kies een attribuut" variant="filled"
+                                        InputProps={{
+                                        ...params.InputProps,           
+                                        }} 
+                                    />
+                                    )}
+                                />
+                                <Accordion sx={{margin: 'auto'}}>
+                                    <AccordionSummary id="panel1" expandIcon={<ArrowDropDownIcon/>} sx={{ margin: 'auto' }}>
+                                        kleur
+                                    </AccordionSummary>
+                                        <AccordionDetails sx={{ margin: 'auto' }}>
+                                        {colorArray.length === 0 ? (
+                                        <p>Kies een kolom met kleur</p>
+                                            ) : (
+                                                <ul>
+                                                {colorArray.map((color, index) => (
+                                                    <li key={index}>
+                                                    <button onClick={() => colorClick(color)}>{color}</button>    
+                                                    </li>
+                                                ))}
+                                                </ul>
+                                            )}
+                                        </AccordionDetails> {/* moet de values pakken van currentarray */}
+                                </Accordion>
+                                    <Box sx={boxClickStyle}>
+                                    <Button onClick={closeDialog}>Annuleer</Button>
+                                    <Button>Oke</Button>
+                                </Box>
+                            </Box>
+                       </Dialog>
+                        </div>
+
+    <TableContainer component={Paper} style={{ maxHeight: 600, overflowY: 'auto' }}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead style={{ position: 'sticky', top: 0 }}>
+                {/* components */}
+                <TableRow>
+                    <TableCell sx={{ fontWeight:'bold' }} > 
+                        eerste regel product
+                    </TableCell>
+                    {mainArray[index] && mainArray[index][0].map((array, index) => (
+                    <TableCell key={index} sx={{ fontWeight:'bold', textAlign: 'center' }} >
+                        <div>
+                        <Autocomplete
+                            id={`keuzesInput_${index}`}
+                            options={options}
+                            value={inputValues[index] || null}
+                            disableClearable
+                            onChange={(event,newValue,) => handleChange(event,newValue,index)}
+                            getOptionDisabled={(option => disabledOptions.includes(option))}
+                            renderInput={(params) => (
+                                <TextField {...params} label="Kolom matchen of overslaan" variant="filled" style={{ width: 200 }} 
+                                    InputProps={{
+                                    ...params.InputProps,           
+                                    }} 
+                                />
+                                )}
+                            />
+                            
+                            <Button variant="contained" onClick={()=> Overslaan(index)} style={{  marginTop:10 }}> 
+                            Overslaan
+                            </Button>                           
+                        </div>
+                    </TableCell>
+                    ))}
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {/* rows */}
+                {mainArray[index] && mainArray[index]
+                .filter(row=>row.length > 0, _.remove(rows, row => row.length === 0))
+                .map((row, rowIndex) => (
+                        <TableRow key={rowIndex}>
+                            <TableCell> 
+                                <input type="radio" name="group" id="selected" checked={selectedRow === rowIndex} onChange={()=> handleClick(index,rowIndex)} />                    
+                            </TableCell>
+                            {row.map((cell, cellIndex) => (
+                                <TableCell key={cellIndex} style={{textAlign: 'center', ...cellStyle(disableTable,disabledRows,rowIndex,currentOptions,processedData,cellIndex)}} > 
+                                    {cell ?? ''} 
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    </TableContainer>
+
+    <button onClick={reset}>terug</button>
+    <button onClick={()=>toggleTable('prev')} disabled={index === 0}>vorige file</button>
+    <button onClick={()=>toggleTable('next')} disabled={index === count -1}>volgende file</button>
+    <button onClick={toDatabase}>naar database</button>
+    <button onClick={toResult}>showresult</button>
+                </div>
+            </div>
     </div>
-  );
+  )
 }
 
 export default Tabel;
